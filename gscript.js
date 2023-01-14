efda66ce2bd7cafc154ed13e07a792f8ad689182
Adzei Setutsi Yao <setutsi.adzei@amalitech.org>
2023-01-14 23:37:41 +0000

    started working on the users option

--- gscript.js
+++ gscript.js
@@ -2,10 +2,33 @@ let counter = 0;
 let firstSelection = "";
 let secondSelection = "";
 
+// Obtaining the Parameters
+let parameters = new URLSearchParams(window.location.search)
+
+// Obtaining the theme Parameters
+let themeParameter = parameters.get("theme")
+
+// Obtaining the number-of-players Parameters
+let numberOfPlayersParameter = parameters.get("numberOfPlayers")
+
+// Obtaining the grid-size Parameters
+let gridSizeParameter = parameters.get("gridSize")
+
+// Function to load what the player selected
+const loadIcon4x4 = (theme,grid) => {
+    if ( theme === "Icons" && grid === "4x4") {
+        document.getElementById("icon-4x4").style.opacity = "1"
+    }
+}
+window.addEventListener("load", function(){
+    loadIcon4x4(themeParameter, gridSizeParameter)
+});
+
+
+
 const icons = document.querySelectorAll("#icons .icon");
 icons.forEach((icon) => {
     icon.onclick = function(){
-        // console.log(true);
         icon.classList.add("clicked");
 
         if(counter === 0){
@@ -17,14 +40,12 @@ icons.forEach((icon) => {
 
             if(firstSelection === secondSelection){
                 const correctIcons = document.querySelectorAll(".icon[icon='" + firstSelection + "']");
-                // icon.getAttribute("icon")
     
                 correctIcons[0].classList.add("correct");
                 correctIcons[0].classList.remove("clicked");
                 correctIcons[1].classList.add("correct");
                 correctIcons[1].classList.remove("clicked");
             } else {
-                // const incorrectIcons = document.querySelectorAll(".icon[icon='" + secondSelection + "']");
                 const incorrectIcons = document.querySelectorAll(".icon.clicked")
     
                 incorrectIcons[0].classList.add("incorrect");
@@ -47,3 +68,16 @@ icons.forEach((icon) => {
     }
     
 })
+
+/*
+<i class="fa-solid fa-0"></i>
+<i class="fa-solid fa-1"></i>
+<i class="fa-solid fa-2"></i>
+<i class="fa-solid fa-3"></i>
+<i class="fa-solid fa-4"></i>
+<i class="fa-solid fa-5"></i>
+<i class="fa-solid fa-6"></i>
+<i class="fa-solid fa-7"></i>
+<i class="fa-solid fa-8"></i>
+<i class="fa-solid fa-9"></i>
+*/
